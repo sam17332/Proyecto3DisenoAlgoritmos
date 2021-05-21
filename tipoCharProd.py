5cebce4c9265f806a9bf5133d36f1c90733b179b
toti <sam17332@uvg.edu.gt>
2021-05-20 23:04:30 -0600

    Clase para manejar los elementos de una produccion

---
+++ tipoCharProd.py
@@ -0,0 +1,40 @@
+class TipoCharProd:
+    def __init__(self):
+        self.tipo = ""
+        self.valor = ""
+        self.numToken = ""
+        self.parametros = ""
+        self.primeraPos = []
+
+    def getTipo(self):
+        return self.tipo
+
+    def setTipo(self, tipo):
+        self.tipo = tipo
+
+    def getValor(self):
+        return self.valor
+
+    def setValor(self, valor):
+        self.valor = valor
+
+    def getNumToken(self):
+        return self.numToken
+
+    def setNumToken(self, numToken):
+        self.numToken = numToken
+
+    def getParametros(self):
+        return self.parametros
+
+    def setParametros(self, param):
+        self.parametros = param
+
+    def getPrimeraPos(self):
+        return self.primeraPos
+
+    def setPrimeraPos(self, pos):
+        self.primeraPos.append(pos)
+
+    def getTipoCharProd(self):
+        return [self.tipo, self.valor, self.numToken, self.parametros, self.primeraPos]
