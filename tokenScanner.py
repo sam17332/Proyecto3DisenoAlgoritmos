e12979442b1cc8be09f88272d6963a76f26ba435
toti <sam17332@uvg.edu.gt>
2021-05-24 21:38:25 -0600

    Se agrega propiedad id.

--- tokenScanner.py
+++ tokenScanner.py
@@ -2,6 +2,7 @@ class TokenScanner:
     def __init__(self):
         self.tipo = ""
         self.valor = ""
+        self.id = ""
 
     def getTipo(self):
         return self.tipo
@@ -9,11 +10,17 @@ class TokenScanner:
     def setTipo(self, tipo):
         self.tipo = tipo
 
+    def getID(self):
+        return self.id
+
+    def setID(self, id):
+        self.id = id
+
     def getValor(self):
         return self.valor
 
     def setValor(self, valor):
         self.valor = valor
 
-    def getTipoCharProd(self):
-        return [self.tipo, self.valor]
+    def getTokenScanner(self):
+        return [self.tipo, self.valor, self.id]
